e8ac8373a2f3bb736e6231a03856816180e4a7d0
N02R <nh9338743@gmail.com>
2026-07-06 19:01:18 +0300

    update index.php

--- index.php
+++ index.php
@@ -359,8 +359,8 @@
    <script src="assets/js/bootstrap.bundle.min.js"></script>
   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>-->
   <script src="assets/js/all.min.js"></script>
-  <script src="assets/js/main.js"></script>
-<script src="assets/js/wordpress.js"></script>
+  <script src="assets/js/wordpress.js"></script>
+<script src="assets/js/main.js"></script>
 </body>
 
 </html>
