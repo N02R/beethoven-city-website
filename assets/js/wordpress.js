a6f02b5cc4bf7dcb3e441d2958435a5e0048a1a8
N02R <nh9338743@gmail.com>
2026-07-06 19:03:56 +0300

    update wordpress.js

--- assets/js/wordpress.js
+++ assets/js/wordpress.js
@@ -1,15 +1,16 @@
-const WordPressAPI = {
-  
-  baseURL: "http://127.0.0.1:8000/wp-json",
-  
-  // 1. معلومات الموقع
-  getSiteInfo: async function() {
-    try {
-      const res = await fetch(this.baseURL);
-      return await res.json();
-    } catch (err) {
-      console.log("SiteInfo Error:", err);
-    }
-  }
-  
-};
+fetch("http://127.0.0.1:8000/wp-json")
+  .then(r => r.text())
+  .then(data => {
+    document.body.innerHTML += `
+      <div style="position:fixed;bottom:0;left:0;background:green;color:white;padding:10px;z-index:9999">
+        CONNECTED ✔
+      </div>
+    `;
+  })
+  .catch(err => {
+    document.body.innerHTML += `
+      <div style="position:fixed;bottom:0;left:0;background:red;color:white;padding:10px;z-index:9999">
+        ERROR ❌
+      </div>
+    `;
+  });
